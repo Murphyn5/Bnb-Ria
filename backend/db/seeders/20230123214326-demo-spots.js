9d8170974531b9913b6af55f93aa10a0c0531e05
Murphyn5 <nlimurphy@gmail.com>
2023-01-23 15:19:42 -0800

    added seeder for Reviews and Bookings and updated Readme

--- backend/db/seeders/20230123214326-demo-spots.js
+++ backend/db/seeders/20230123214326-demo-spots.js
@@ -59,7 +59,7 @@ module.exports = {
      options.tableName = 'Spots';
      const Op = Sequelize.Op;
      return queryInterface.bulkDelete(options, {
-       name: { [Op.in]: ['App Academy', 'Oscars Trash Can', "Dexter's Laboratory"] }
+       id: { [Op.in]: [1, 2, 3] }
      }, {});
   }
 };
