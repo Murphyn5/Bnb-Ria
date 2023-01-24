7eb97e642d1121bb55c45fae5f2103d1b80316dd
Murphyn5 <nlimurphy@gmail.com>
2023-01-24 12:43:38 -0800

    added preview image to response

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -1,7 +1,7 @@
 const express = require('express')
 
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
-const { User, Spot, Booking } = require('../../db/models');
+const { Spot, Booking, SpotImage } = require('../../db/models');
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 
@@ -12,29 +12,48 @@ const router = express.Router();
 //Get Current User Bookings
 
 router.get(
-  '/current',
-  requireAuth,
-  async (req, res) => {
-    const bookings = await Booking.findAll({
-        where: {
-            userId: req.user.id
-        },
-        include: [
-            {
-                model: Spot
-            },
-            {
-                model: User
+    '/current',
+    requireAuth,
+    async (req, res) => {
+        const bookings = await Booking.findAll({
+            where: {
+                userId: req.user.id
             }
-        ]
-    })
+        })
 
-    console.log(bookings)
+        const bookingsPOJO = []
 
-    return res.json(({
-        Bookings: bookings
-    }))
-  }
+        for (let i = 0; i < bookings.length; i++) {
+
+            let booking = bookings[i]
+            const bookingPOJO = await booking.toJSON()
+
+            const spot = await Spot.findByPk(bookingPOJO.spotId)
+
+            const spotPOJO = await spot.toJSON()
+
+            //adds to previewImage
+
+            const spotImage = await SpotImage.findOne({
+                where: {
+                    spotId: spotPOJO.id
+                }
+            })
+
+
+            if (spotImage.preview === true) {
+                spotPOJO.previewImage = spotImage.url
+            }
+
+            bookingPOJO.Spot = spotPOJO
+
+            bookingsPOJO.push(bookingPOJO)
+        }
+
+        return res.json(({
+            Bookings: bookingsPOJO
+        }))
+    }
 
 
 )
