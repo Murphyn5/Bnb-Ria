9b3a3f86cde2652702d43a764bb47a3ccc4f9564
Murphyn5 <nlimurphy@gmail.com>
2023-02-17 10:36:14 -0800

    updated spot seeder

--- backend/db/seeders/20230124172930-demo-spot-image.js
+++ backend/db/seeders/20230124172930-demo-spot-image.js
@@ -117,7 +117,7 @@ module.exports = {
     options.tableName = 'SpotImages';
     const Op = Sequelize.Op;
     return queryInterface.bulkDelete(options, {
-      id: { [Op.in]: [1, 2, 3] }
+      id: { [Op.in]: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20] }
     }, {});
   }
 
